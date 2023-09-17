6275c1911e40bea0132a859872528c3d8d297636
Sandro Gataric <132219558+Sandro2906@users.noreply.github.com>
2023-09-17 22:22:22 +0200

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
                 He has one year left until graduation. I hope to be a full stack developer in the future.
             </p>
             <div>
-                <Link to='portfolio' smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-400 to-white cursor-pointer'>
+                <Link to='portfolio' smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-400 to-cyan-800 cursor-pointer'>
                     Portfolio <span className='group-hover:rotate-90 duration-150'> <MdKeyboardArrowRight size={25} className='ml-1'/> </span> 
                 </Link>
             </div>
